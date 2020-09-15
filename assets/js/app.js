d0b64e29c0d70ebaecf48633d9913ca618e997f4
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-09-15 15:07:13 +0100

    A little modification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 const bodyBgd = document.querySelector('.mainBody')
-const arrayOfColor = ['brown','#000', '#F8A300','#007BBC','#D54F46','#0366D6','#FEFEFE','yellow','gold','grey','chartreuse']
+const arrayOfColor = ['brown','#000', '#F8A300','#007BBC','#D54F46','navy','#0366D6','#FEFEFE','yellow','gold','grey','chartreuse']
 const colorName = document.querySelector('.colorName')
 const generateBtn = document.querySelector('.btn').addEventListener('click', () => {
-   const randomColor = arrayOfColor[Math.floor(Math.random() * arrayOfColor.length)]
+  //  const randomColor = arrayOfColor[Math.floor(Math.random() * arrayOfColor.length)]
+  const randomColor = arrayOfColor[Math.floor(Math.random() * arrayOfColor.length)]
    bodyBgd.style.background = randomColor
    colorName.style.color = 'pink'
    colorName.textContent = randomColor
+  //  console.log(randomColor)
 })
