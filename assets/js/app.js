5178593a4e73c4b5005dd3e8b91f96fd747dc91d
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-09-17 11:23:28 +0100

    I modified the app.js code to accept rgb color

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 const bodyBgd = document.querySelector('.mainBody')
-const arrayOfColor = ['brown','#000', '#F8A300','#007BBC','#D54F46','navy','#0366D6','#FEFEFE','yellow','gold','grey','chartreuse']
+// const arrayOfColor = ['brown','#000', '#F8A300','#007BBC','#D54F46','navy','#0366D6','#FEFEFE','yellow','gold','grey','chartreuse']
 const colorName = document.querySelector('.colorName')
 const generateBtn = document.querySelector('.btn').addEventListener('click', () => {
   //  const randomColor = arrayOfColor[Math.floor(Math.random() * arrayOfColor.length)]
-  const randomColor = arrayOfColor[Math.floor(Math.random() * arrayOfColor.length)]
-   bodyBgd.style.background = randomColor
-   colorName.textContent = randomColor
-  //  console.log(randomColor)
+  const red = Math.floor(Math.random() * 256) + 0
+  const green = Math.floor(Math.random() * 256) + 0
+  const blue = Math.floor(Math.random() * 256) + 0
+  bodyBgd.style.background = `rgb(${red},${green},${blue})`
+  colorName.textContent = `rgb(${red},${green},${blue})`
 })
